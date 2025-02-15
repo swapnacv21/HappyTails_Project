962103762ff577d618b73d1d3e1fed9e35c1141f
swapnacv21 <swapnavenu116@gmail.com>
2025-02-16 02:43:13 +0530

    project started

--- happy_tails/app/urls.py
+++ happy_tails/app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns=[
     path('',views.shop_login),
+    path('shop_logout',views.shop_logout),
     path('shop_home',views.shop_home),
     path('add_pet',views.add_pet),
     path('dog_list/<id>',views.dog_list),
